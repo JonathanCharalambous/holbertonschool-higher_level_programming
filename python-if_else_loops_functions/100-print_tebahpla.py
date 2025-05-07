ba593e76395ca014de847728c53e5209d9bbdab6
JonathanCharalambous <103863795+JonathanCharalambous@users.noreply.github.com>
2025-05-07 23:31:08 +1000

    Update 100-print_tebahpla.py

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 for i in range(25, -1, -1):
-    print("{}".format(chr(i + 65)) if i % 2 == 0 
+    print("{}".format(chr(i + 65)) if i % 2 == 0
         else "{}".format(chr(i + 97)), end='')
