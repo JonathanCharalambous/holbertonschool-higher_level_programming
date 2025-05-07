8e04938e2d900afd621e539e1828580ce5ea7d5b
JonathanCharalambous <103863795+JonathanCharalambous@users.noreply.github.com>
2025-05-07 22:58:54 +1000

    Update 3-print_alphabt.py

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 alphabet = ""
 for i in range(ord('a'), ord('z') + 1):
-        if i != ord('e') and i != ord('q'):
-                alphabet += chr(i)
-print("{}".format(alphabet), end='')
+    if i != ord('e') and i != ord('q'):
+        alphabet += chr(i)
+print("{}".format(alphabet), end='')  
