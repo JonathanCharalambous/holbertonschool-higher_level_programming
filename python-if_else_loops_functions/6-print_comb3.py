824327de71362a3fc44d4f374659f144d328ea74
JonathanCharalambous <103863795+JonathanCharalambous@users.noreply.github.com>
2025-05-07 23:09:59 +1000

    Update 6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -5,4 +5,5 @@ for i in range(10):
         num1 = i
         num2 = j
         if num1 < num2:
-            print("{}{}".format(num1, num2), end=", " if not (num1 == 8 and num2 == 9) else "\n")
+            print("{}{}".format(num1, num2), end=", " 
+                  if not (num1 == 8 and num2 == 9) else "\n")
