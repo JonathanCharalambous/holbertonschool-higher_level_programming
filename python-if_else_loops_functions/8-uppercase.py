370cb91b08b880d0b0927f25938b2a589498b43f
JonathanCharalambous <103863795+JonathanCharalambous@users.noreply.github.com>
2025-05-07 23:41:44 +1000

    Update 8-uppercase.py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -4,4 +4,4 @@ def uppercase(c):
     for i in range(len(c)):
         if ord(c[i]) >= 97 and ord(c[i]) <= 122:
             c = c[:i] + chr(ord(c[i]) - 32) + c[i + 1:]
-    print("{}".format(c), end='')
+    print("{}".format(c), end='\n')
