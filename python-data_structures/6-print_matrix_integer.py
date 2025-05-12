5565c005767389dd8fab75ff98eac8664adab9b6
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-05-12 12:39:06 +1000

    DataStuctueDone

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    if matrix is None:
+        return None
+    for row in matrix:
+        for i in range(len(row)):
+            if i == len(row) - 1:
+                print("{}".format(row[i]), end="")
+            else:
+                print("{}".format(row[i]), end=" ")
