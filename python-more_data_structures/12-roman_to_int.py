45d5503de4c7219c81aca80eff4bcce91b9d6cb0
JonathanCharalambous <103863795+JonathanCharalambous@users.noreply.github.com>
2025-05-13 23:30:55 +1000

    Update 12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
-    
+
     roman_numerals = {
         'I': 1,
         'V': 5,
