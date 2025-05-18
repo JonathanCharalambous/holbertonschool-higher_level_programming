b9e1f4821a3c6a56e759111eec948d3a834f4f24
JonathanCharalambous <103863795+JonathanCharalambous@users.noreply.github.com>
2025-05-18 17:40:00 +1000

    Update 3-say_my_name.py

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,26 @@
 #!/usr/bin/python3
+"""
+This module provides the say_my_name function that
+prints a message with the provided strings.
+"""
+
 
 def say_my_name(first_name, last_name=""):
+    """
+    Prints a message with the two provided arguments.
+
+    Args:
+        first_name: First name to print.
+        last_name: Last name to print.
+
+    Returns:
+        No return
+
+    Raises:
+        TypeError: If first name is not a string.
+        TypeError: Iflast name is not a string.
+        
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
