238ff1a9874edcc2755aa15b81a95b8ef687d9ea
JonathanCharalambous <103863795+JonathanCharalambous@users.noreply.github.com>
2025-05-18 19:06:18 +1000

    Update 5-text_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python3
+"""
+This module provides the text_indentation function that
+a provided text with indentations added.
+"""
+
 
 def text_indentation(text):
+    """
+    Prints a provided text with indentations added.
 
+    Args:
+        text: The text to indent.
+
+    Returns:
+        No return
+
+    Raises:
+        TypeError: If text is not an string.
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    
-    
+
     for letter in text:
         print(letter, end="")
         if letter in ".?:":
