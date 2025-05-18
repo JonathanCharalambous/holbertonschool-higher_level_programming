c9e78a5de931161dfa4a65276050a2ea3163643a
JonathanCharalambous <103863795+JonathanCharalambous@users.noreply.github.com>
2025-05-18 17:48:04 +1000

    Update 3-say_my_name.py

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -25,6 +25,6 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    if last_name is not None
+    if last_name:
         last_name = " " + last_name
     print("My name is {}{}".format(first_name, last_name))
