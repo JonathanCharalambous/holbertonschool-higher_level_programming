c1860a78a96dd1518c3dda9046489e9df4c0c8a9
JonathanCharalambous <103863795+JonathanCharalambous@users.noreply.github.com>
2025-05-18 18:43:24 +1000

    Update 4-print_square.py

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,7 +1,24 @@
 #!/usr/bin/python3
-
+    """
+    This module provides the say_my_name function that
+    prints a message with the provided strings.
+    """
 def print_square(size):
+    """
+    Prints a message with the two provided arguments.
 
+    Args:
+        first_name: First name to print.
+        last_name: Last name to print.
+
+    Returns:
+        No return
+
+    Raises:
+        TypeError: If first name is not a string.
+        TypeError: Iflast name is not a string.
+
+    """
     if not isinstance(size, int):
         raise TypeError("Size must be an integer")
     if size < 0:
