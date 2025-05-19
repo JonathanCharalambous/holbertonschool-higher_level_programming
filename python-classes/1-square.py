79033cc756aa0c110dcb57ed816451abfd1198d7
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-05-19 11:00:16 +1000

    square done

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+This module defines an empty class Square used to represent a square.
+"""
+
+
+class Square:
+
+    """This class defines a square."""
+    def __init__(self, size):
+        self.__size = size
