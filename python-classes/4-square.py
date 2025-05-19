fece9b67ad5ba435bbf209d7492fe20e0f72289f
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-05-19 12:34:28 +1000

    Create 4-square.py

---
+++ python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+    This module defines a class Square used to represent a square.
+"""
+
+
+class Square:
+
+    """This class defines a square."""
+    def __init__(self, size=0):
+        """Initialize a new Square instance."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Calculate and return the current square area."""
+        return (self.__size ** 2)
+    
+    def size(self, size):
+        """Setter for square.size."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
