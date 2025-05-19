7c84ab455a55dca9ef172dfb1968b778a33ef096
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-05-19 10:47:20 +1000

    0-square updated

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 
+"""
+This module provides the add_integer function 
+that defines a square.
+"""
+
+
 class Square:
+
+    """This class defines a square."""
 
     def __int__(self):
         pass
