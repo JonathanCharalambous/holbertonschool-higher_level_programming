7a2d889c56688db4b897dc6532690fd093c48766
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-05-19 21:30:22 +1000

    0-rectangle created

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+This module defines an empty class Rectangle that is
+used to represent a rectangle.
+"""
+
+
+class Rectangle:
+
+    """This class defines a blank Rectangle."""
+    
