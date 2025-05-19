00bf66257eebc042dcdd010a61cad0702d6fb0b1
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-05-19 21:57:15 +1000

    Update 1-rectangle.py

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -22,12 +22,12 @@ class Rectangle:
         if height < 0:
             raise ValueError("height must be >= 0")
         self.__height = height
-    
+
     @property
     def width(self):
         """Getter for Rectangle.width."""
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         """Setter for Rectangle.width."""
@@ -36,15 +36,15 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-    
+
     @property
     def height(self):
         """Getter for Rectangle.height."""
         return self.__height
-    
+
     @height.setter
     def height(self, value):
-        """Getter for Rectangle.height."""
+        """Getter for Rectangle.height"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
