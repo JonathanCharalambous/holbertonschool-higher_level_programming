b3ebe1fab1eba6f52ad0f20d902399b535fd5b23
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-05-19 22:48:23 +1000

    Update 3-rectangle.py

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -71,5 +71,5 @@ class Rectangle:
     def __str__(self):
         """Prints the rectangle with the character #"""
         if self.__width == 0 or self.__height == 0:
-            return "" 
+            return ""
         return "\n".join(["#" * self.__width for _ in range(self.__height)])
