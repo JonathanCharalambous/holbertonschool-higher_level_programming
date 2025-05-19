f717b489abe501debfbb2ba2e4db2970c6aeca61
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-05-19 22:15:58 +1000

    Update 2-rectangle.py

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -57,4 +57,6 @@ class Rectangle:
     
     def perimeter(self):
         """Returns the area for a Rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
         return (self.__width * 2 + self.__height * 2)
