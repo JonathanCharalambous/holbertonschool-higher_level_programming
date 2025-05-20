35a7218670a679ab88909210c40ac27c55b24a75
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-05-20 11:45:21 +1000

    Update 7-rectangle.py

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -74,9 +74,10 @@ class Rectangle:
 
     def __str__(self):
         """Prints the rectangle with the character #"""
+        symbol = str(self.print_symbol)
         if self.__width == 0 or self.__height == 0:
             return ""
-        return "\n".join([str(self.print_symbol) * self.__width for _ in range(self.__height)])
+        return "\n".join([symbol * self.__width for _ in range(self.__height)])
 
     def __repr__(self):
         """Return a string representation of the rectangle."""
