de3aa301fda781dd8c6a56f82788e7da4a52f50d
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-05-26 15:55:02 +1000

    Update 6-base_geometry.py

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
+"""
+This module defines a class that is
+used to represent BaseGeometry.
+"""
+
 
 class BaseGeometry:
 
     def __init__(self):
+        """Initializes the BaseGeometry class."""
         pass
 
     def area(self):
-
+        """Calculates the area of a geometry."""
         raise Exception("area() is not implemented ")
