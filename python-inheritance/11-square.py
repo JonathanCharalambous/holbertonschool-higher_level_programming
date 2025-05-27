304161a45668a22ba98162d60ed3343a02ac369c
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-05-27 15:33:14 +1000

    Create 11-square.py

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This module defines a class that is
+used to represent a Square which implements Rectangle.
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """"
+    class that is used to represent
+    a Square which implements Rectangle
+    """""
+    def __init__(self, size):
+        """Initializes the Square class."""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Calculates the area of the square."""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """Returns a string representation of the square."""
+        return "[Square] {}/{}".format(self.__size, self.__size)
