60ed3de1dcb05909b638fbba129f1cd7c585cf13
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-05-28 10:42:54 +1000

    task 1

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+from abc import ABC, abstractmethod
+import math
+"""
+This module defines an abstract base class 
+for shapes and concrete implementations for Circle and Rectangle.
+"""
+
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        """Calculate the area of the shape."""
+        pass
+    @abstractmethod
+    def perimeter(self):
+        """Calculate the perimeter of the shape."""
+        pass
+
+class Circle(Shape):
+    """Concrete implementation of Shape for circles."""
+    def __init__ (self, radius):
+        """Initialize the circle"""
+        self.radius = radius
+
+    def area(self):
+        """Calculate the area of the circle."""
+        return math.pi * self.radius ** 2
+    
+    def perimeter(self):
+        """Calculate the perimeter of the circle."""
+        return 2 * math.pi * self.radius
+
+class Rectangle(Shape):
+    """Concrete implementation of Shape for rectangles."""
+    def __init__ (self, width, height):
+        """Initialize the rectangle"""
+        self.width = width
+        self.height = height
+
+    def area(self):
+        """Calculate the area of the rectangle."""
+        return self.width * self.height
+    
+    def perimeter(self):
+        """Calculate the perimeter of the rectangle."""
+        return 2 * (self.width + self.height)
