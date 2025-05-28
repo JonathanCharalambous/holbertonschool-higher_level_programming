8ff1643a6aa9f954e5333d17b94b6c18bc7f3f1f
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-05-28 11:48:41 +1000

    task 2

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+This module defines an VerboseList class 
+for a list and prints messages with actions pulled
+from the super class.
+"""
+
+
+class VerboseList(list):
+    """A list that prints messages when items are added or removed."""
+
+    def append(self, item):
+        """Append an item to the list and print a message."""
+        super().append(item)
+        print("Appended {} to the list.".format(item))
+
+    def extend(self, iterable):
+        """Extend the list with items from an iterable and print a message."""
+        super().extend(iterable)
+        print("Extended the list with {}.".format(list(iterable)))
+
+    def remove(self, item):
+        """Remove an item from the list and print a message."""
+        super().remove(item)
+        print("Removed {} from the list.".format(item))
+    
+    def pop(self, index=-1):
+        """Pop an item from the list at the given index and print a message."""
+        item = super().pop(index)
+        print("Popped {} from the list.".format(item))
