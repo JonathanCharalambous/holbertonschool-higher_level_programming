8b4d6f0d44951d3a99be4744f5058c0a6d2122eb
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-06-08 16:18:12 +1000

    Update 11-student.py

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -6,7 +6,7 @@ converts a Student object to its JSON representation
 
 
 class Student:
-    """A class that defines a student by their first name, last name, and age."""
+    """class that defines a student by their first name, last name, and age"""
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
@@ -17,8 +17,8 @@ class Student:
         if attrs is None:
             return self.__dict__
         return {key: self.__dict__[key] for key in attrs}
-    
+
     def reload_from_json(self, json):
-        """Replaces all attributes of the Student instance with those from json"""
+        """Replaces attributes of the Student instance with those from json"""
         for key, value in json.items():
             setattr(self, key, value)
