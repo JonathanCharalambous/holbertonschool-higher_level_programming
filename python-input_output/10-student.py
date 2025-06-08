0652242adc317d0245aed89c42ad525479894592
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-06-08 16:15:12 +1000

    Update 10-student.py

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -16,4 +16,4 @@ class Student:
         """Returns the dictionary representation of the Student instance"""
         if attrs is None:
             return self.__dict__
-        return {key: self.__dict__[key] for key in attrs}
+        return {key: self.__dict__[key] for key in attrs if key in self.__dict__}
