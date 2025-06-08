9002fd0907b3393cd6b31490e7bfb4fa6a10d920
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-06-08 16:02:42 +1000

    Update 2-append_write.py

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -6,6 +6,9 @@ appends to a text file (UTF8) and returns the number of characters written
 
 
 def append_write(filename="", text=""):
-    """Appends a string to a text file (UTF8) and returns the number of characters added"""
+    """
+    Appends a string to a text file (UTF8)
+    and returns the number of characters added
+    """
     with open(filename, "a", encoding="utf-8") as file:
         return file.write(text)
