c265fe4bbce33b8d690e109a4593a1484df2f2f9
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-06-11 15:46:12 +1000

    Update task_04_flask.py

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -2,13 +2,7 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-users_dict = {
-        "jane": {
-            "name": "Jane Doe",
-            "age": 28,
-            "city": "Los Angeles"
-        },
-    }
+users_dict = {}
 
 @app.route('/')
 
