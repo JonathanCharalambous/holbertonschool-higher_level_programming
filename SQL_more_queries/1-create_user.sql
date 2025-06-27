bea6cc7f10266d7f7122dbac50f50fc054325b45
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-06-27 12:44:03 +1000

    Update 1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,3 @@
 -- script to create a user with a password 
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES on *.* TO 'user_0d_1'@'localhost';
