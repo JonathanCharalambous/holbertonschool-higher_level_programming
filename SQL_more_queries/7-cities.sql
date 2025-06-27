84b7429f4699b794c91b489fb8895457d5cdcfad
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-06-27 18:35:58 +1000

    Create 7-cities.sql

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,11 @@
+-- script to create a database and a table with a primary key
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS cities (
+    id int AUTO_INCREMENT,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+    FOREGIN KEY (state_id) REFERENCES states(id)
+);
