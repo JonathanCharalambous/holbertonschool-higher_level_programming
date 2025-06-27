4233ed05b5cf30e20802d938adaa9110e6e6cb04
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-06-27 15:23:37 +1000

    Create 4-never_empty.sql

---
+++ SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,6 @@
+-- script to create a table with deafault value for id
+CREATE TABLE IF NOT EXISTS id_not_null (
+    id INT NOT NULL DEFAULT 1,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+)
