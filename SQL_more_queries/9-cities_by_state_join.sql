3159fb2f91134218230931090c89be25ac893f1f
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-06-27 18:58:26 +1000

    Update 9-cities_by_state_join.sql

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,4 @@
 -- Script to find all info about cities in California using JOIN
 SELECT cities.id, cities.name, states.name
 FROM cities
-JOIN states ON cities.state_id = states.id
-WHERE states.name = 'California';
+JOIN states ON cities.state_id = states.id;
