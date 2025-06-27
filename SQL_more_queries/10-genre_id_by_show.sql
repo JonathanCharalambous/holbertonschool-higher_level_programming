77d88e816b986e796abf060cf2cae3a98d2141d7
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-06-27 19:29:43 +1000

    Create 10-genre_id_by_show.sql

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,5 @@
+-- Script to select title and genre id based off the id
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
