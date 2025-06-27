6857f892e4b880ce3c37c623847e853e15c89de3
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-06-27 13:21:09 +1000

    Create 2-create_read_user.sql

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- script to create a user with read-only access
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
