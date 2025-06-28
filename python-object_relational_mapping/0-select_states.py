1a690111c20851b84929248520ca344b59758e03
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-06-28 19:29:36 +1000

    Update 0-select_states.py

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-# script that lists all states from the database hbtn_0e_0_usa
+# script that lists all states from the database hbtn_0d_usa
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-
+    # script that lists all states from the database hbtn_0d_usa
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
