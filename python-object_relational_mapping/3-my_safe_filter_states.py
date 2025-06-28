347f6610d3f45946f62fdbc0b77326c38a7f92c2
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-06-28 23:13:17 +1000

    Create 3-my_safe_filter_states.py

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""script that lists all states from the db hbtn_0d_usa"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    search = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database,
+    )
+
+    cursor = db.cursor()
+    cursor.execute(
+        "SELECT * FROM states WHERE name = %s ORDER BY states.id ASC",
+        (sys.argv[4], ))
+
+    rows = cursor.fetchall()
+
+    for result in rows:
+        print(result)
