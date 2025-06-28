45ad2a6044727a8dadc27ebfeed9ff59824042c8
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-06-28 22:08:47 +1000

    Create 2-my_filter_states.py

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""script that lists all states from the db hbtn_0d_usa"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    search = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database,
+    )
+
+    cursor = db.cursor()
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(search)
+    cursor.execute(query)
+
+    rows = cursor.fetchall()
+
+    for result in rows:
+        print(result)
