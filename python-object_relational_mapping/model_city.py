db919d1caa4875f3eeb0d219ece644addd4dfca7
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-06-30 15:50:59 +1000

    task14

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""""Define a City class that creates a table for ORM"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """City class for ORM"""
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, nullable=False, ForeignKey=("states.id"))
