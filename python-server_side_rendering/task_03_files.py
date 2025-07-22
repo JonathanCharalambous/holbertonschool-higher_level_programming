cf42e17bbea89777ac187555a4bb4b8b7719fbeb
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-07-22 22:27:54 +1000

    task3

---
+++ python-server_side_rendering/task_03_files.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request
+import json
+import csv
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/items')
+def data():
+    source = request.args.get('source')
+    record_id = request.args.get('id')
+
+    data_list = []
+
+    if source == 'json':
+        try:
+            with open('products.json') as f:
+                data_list = json.load(f)
+        except Exception as e:
+            return f"Error loading JSON: {e}", 500
+
+    elif source == 'csv':
+        try:
+            with open('products.csv') as f:
+                reader = csv.DictReader(f)
+                data_list = list(reader)
+        except Exception as e:
+            return f"Error loading CSV: {e}", 500
+
+    else:
+        return "Invalid source. Use ?source=json or ?source=csv", 400
+    
+    return render_template("items.html", items=data_list)
+    
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
