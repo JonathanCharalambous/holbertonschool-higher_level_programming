31001a7b3830d5934fce7ed461134f5f130a127c
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-07-22 11:05:18 +1000

    Update task_00_intro.py

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -1,8 +1,11 @@
 
 def generate_invitations(template, attendees):
+    
+    inv_num = 0
+    if not isinstance(template, str):
+        raise TypeError("Template must be a string")
+
     for attendee in attendees:
-        if not isinstance(template, str):
-            raise TypeError("Template must be a string")
         
         invitation = template.format(
             name=attendee.get("name", "N/A"),
@@ -10,5 +13,8 @@ def generate_invitations(template, attendees):
             event_date=attendee.get("event_date", "N/A"),
             event_location=attendee.get("event_location", "N/A")
         )
+        inv_num += 1
+        with open(f"output_{inv_num}.txt", "w") as f:
+            f.write(invitation)
 
         print(invitation)
